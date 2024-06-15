68f8acd81537e7249728756eac0ec9ce104dfd5a
Wolfgang Schoch <git@wolfgangschoch.de>
2024-06-15 21:56:02 +0200

    character generation basic work

--- backend/services/openai_service.py
+++ backend/services/openai_service.py
@@ -11,7 +11,7 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 def load_prompt(template_name, keywords):
     with open(f'backend/prompts/{template_name}', 'r') as file:
         prompt = file.read()
-    return prompt.replace('{keywords}', keywords)
+    return prompt.replace('{template}', keywords)
 
 def load_schema(schema_name):
     with open(f'backend/schemas/{schema_name}', 'r') as file:
@@ -20,6 +20,42 @@ def load_schema(schema_name):
 def generate_case(keywords):
     prompt = load_prompt('scenario.txt', keywords)
     schema = load_schema('case.json')
+
+    response = openai.chat.completions.create(
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": prompt}
+        ],
+    tools=[
+        {
+            "type": "function",
+            "function": schema,
+        }
+    ],
+    tool_choice="required",
+    model="gpt-4o",
+    )
+
+    scenario = json.loads(response.choices[0].message.tool_calls[0].function.arguments)
+    characters = generate_character_details(response.choices[0].message.tool_calls[0].function.arguments)
+
+
+    # Füge die Charakterdetails zum Szenario hinzu
+    for character in characters['characters']:
+        for scenario_character in scenario['characters']:
+            if character['name'] == scenario_character['name']:
+                scenario_character.update(character)
+
+
+    # Ausgabe des gemergten Ergebnisses
+    print(json.dumps(scenario, indent=2))
+
+
+    return json.loads(scenario)
+
+def generate_character_details(scenario):
+    prompt = load_prompt('character.txt', scenario)
+    schema = load_schema('character.json')
 
     response = openai.chat.completions.create(
     messages=[
